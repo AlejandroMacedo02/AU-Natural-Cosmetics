401c3be8e591f78bc0fb936dc0b4f4ac374c4790
AlejandroMacedo02 <106896409+AlejandroMacedo02@users.noreply.github.com>
2023-11-07 23:50:50 -0500

    AVANCE 20%

---
+++ src/main/java/upeu/edu/pe/AUNaturalCosmetics/infrastructure/entity/OrderEntity.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package upeu.edu.pe.AUNaturalCosmetics.infrastructure.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ *
+ * @author alejandromacedop
+ */
+@Entity
+@Table(name = "orders")
+public class OrderEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private LocalDateTime dateCreated;
+    private String status;
+    
+    @ManyToOne
+    private UserEntity user;
+    
+    @Transient
+    private List<OrderProductEntity> orderProducts;
+
+    public OrderEntity() {
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public List<OrderProductEntity> getOrderProducts() {
+        return orderProducts;
+    }
+
+    public void setOrderProducts(List<OrderProductEntity> orderProducts) {
+        this.orderProducts = orderProducts;
+    }
+    
+    /*agregar productos a arreglo*/
+     public void addOrdersProduct(List<OrderProductEntity> orderProducts){
+        this.setOrderProducts(orderProducts);
+    }
+    
+    public BigDecimal getTotalOrderPrice(){
+        return getOrderProducts().stream().map(
+                p->p.getTotalPrice()
+        ).reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+}
