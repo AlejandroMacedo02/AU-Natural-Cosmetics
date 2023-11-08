8d70b728e8cac18979eddaa16ef6e57a05314867
AlejandroMacedo02 <106896409+AlejandroMacedo02@users.noreply.github.com>
2023-11-08 00:23:18 -0500

    AVANCE 33%

--- src/main/java/upeu/edu/pe/AUNaturalCosmetics/infrastructure/controller/ProductControllerApi.java
+++ src/main/java/upeu/edu/pe/AUNaturalCosmetics/infrastructure/controller/ProductControllerApi.java
@@ -4,10 +4,81 @@
  */
 package upeu.edu.pe.AUNaturalCosmetics.infrastructure.controller;
 
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import upeu.edu.pe.AUNaturalCosmetics.app.service.ProductService;
+import upeu.edu.pe.AUNaturalCosmetics.infrastructure.entity.ProductEntity;
+import upeu.edu.pe.AUNaturalCosmetics.infrastructure.entity.UserEntity;
+
 /**
  *
  * @author alejandromacedop
  */
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("api")
 public class ProductControllerApi {
-    
+
+    private final ProductService productService;
+    private final Logger log = LoggerFactory.getLogger(ProductControllerApi.class);    
+
+
+    public ProductControllerApi(ProductService productService) {
+        this.productService = productService;
+    }
+
+    //guardar product
+    @PostMapping("/product")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductEntity createProduct(@RequestBody ProductEntity product) throws IOException{
+        log.info("product {}",product);
+       return productService.saveProductApi(product);
+    }
+
+    //ver productos
+    @GetMapping("/product")
+    public Iterable<ProductEntity> showProduct() {
+        UserEntity user = new UserEntity();
+        user.setId(1);
+        return productService.getProductsByUser(user);
+    }
+
+    @GetMapping("/product/{id}")
+    public ProductEntity show(@PathVariable Integer id) {
+        return productService.getProductById(id);
+    }
+
+    @PutMapping("/product/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductEntity editProduct(@RequestBody ProductEntity product, @PathVariable Integer id) throws IOException {
+        ProductEntity productActual = productService.getProductById(id);
+        productActual.setDescription(product.getDescription());
+        productActual.setName(product.getName());
+        productActual.setPrice(product.getPrice());
+        productActual.setUserEntity(product.getUserEntity());
+        return productService.saveProductApi(productActual);
+        // log.info("Product obtenido: {}", product);
+        //model.addAttribute("product", product);
+        //return "admin/products/edit";
+    }
+
+    @DeleteMapping("/product/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable Integer id) {
+        productService.deleteProductById(id);
+        // return "redirect:/admin/products/show";
+    }
+
 }
